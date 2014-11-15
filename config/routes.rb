05f44766235f66919d968cbc8af2db58ed073ace
Bernie Chiu <bernie_chiu@hotmail.com>
2014-11-15 13:58:08 +0800

    Implement CRUD routes for Group

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   root :to => 'high_voltage/pages#show', :id => 'welcome'
+
+  resources :groups
 end
