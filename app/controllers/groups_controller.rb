fa109c0554358b75a4749daa064fd296317c2406
Bernie Chiu <bernie_chiu@hotmail.com>
2014-11-15 15:01:41 +0800

    Allow edit for Group

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -21,6 +21,20 @@ class GroupsController < ApplicationController
     end
   end
 
+  def edit
+    @group = Group.find(params[:id])
+  end
+
+  def update
+    @group = Group.find(params[:id])
+
+    if @group.update(group_params)
+      redirect_to group_path(@group)
+    else
+      render :edit
+    end
+  end
+
   private
 
   def group_params
